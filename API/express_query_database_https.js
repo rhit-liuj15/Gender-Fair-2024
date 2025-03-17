f8b2324acd277bc570c1bb80d95ca9441f42911e
Ethan Liu <liuj15@rose-hulman.edu>
2025-03-17 14:23:02 -0400

    Add API subfolder & API script

---
+++ API/express_query_database_https.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const mysql = require('mysql2')
+const cors = require('cors')
+const https = require('https')
+const fs = require('fs')
+const os = require('os')
+
+
+const connection = mysql.createConnection({
+  host: 'genderfair2024.csse.rose-hulman.edu',
+  user: 'ProcedureRunner',
+  password: 'GenderFairProcedureRunner2024',
+  database: 'PublicFacingData'
+})
+
+connection.connect()
+
+const app = express()
+app.use(cors()) // This is for flutter to work, the exact reason isn't researched
+const port = 443
+
+
+/**
+ * @api {get} Hello
+ * @apiDescription Responds to the '/hello' suffix with the webserver's and the system's uptime.
+ * @apiName Hello
+ * @apiGroup API
+ *
+ */
+
+app.get('/hello', (req, res) => {
+	res.json([
+    "Hello! The database is live.",
+    "The server has been up for " + process.uptime() + " seconds",
+    "The system has been up for " + os.uptime() + " seconds"
+  ])
+})
+
+app.get('/score', (req, res) => {
+	connection.query('CALL GetSchoolScores', (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: 'CALL GetSchoolScoreTest failed' })
+      return
+    }
+    res.json(rows[0])
+	})
+})
+
+
+// 
+app.get('/averages', (req, res) => {
+	connection.query('CALL GetAverages', (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: 'CALL GetAverages failed' })
+      return
+    }
+    res.json(rows[0])
+	})
+})
+
+app.get('/data', (req, res) => {
+  if (req.query.uids.match('^[0-9, ]*$')) {
+    connection.query('CALL GetSchoolData(?)', [req.query.uids], (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: 'CALL GetSchoolData failed: ' + err });
+        return;
+      }
+      res.json(rows[0]);
+    });
+  } else {
+    // If string validation fails, then don't query the database
+    res.status(400).json({ error: "The provided list of UIDs contains illegal characters outside [0-9, ' ' (space), ',' (comma)]"});
+  }
+});
+
+const options = {
+  key: fs.readFileSync('/home/ethan/genderfair2024api/certificate/privkey.pem'),
+  cert: fs.readFileSync('/home/ethan/genderfair2024api/certificate/cert.pem'),
+  ca: fs.readFileSync('/home/ethan/genderfair2024api/certificate/fullchain.pem')
+}
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`HTTPS server running on port ${port}`)
+})
