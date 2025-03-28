4c7b8ef69acf2b6fcd75e44fc34754903fdd804f
Ethan Liu <liuj15@rose-hulman.edu>
2025-03-28 02:08:38 -0400

    Add state abbreviations to metadata

--- Database/Create Table/Create Metadata Table.sql
+++ Database/Create Table/Create Metadata Table.sql
@@ -6,25 +6,35 @@ Group numbers 1-99 can be used for any purpose.
 The following is an example usage:
 
 INSERT INTO `PublicFacingData`.`Metadata` (`GROUP`, `ABBR`, `DESC`) VALUES
-  (0, '1', 'State names & abbreviations'),
-  (0, '67', 'Companies with 2-letter abbreviations'),
-  (1, 'CA', 'California'),
-  (1, 'NY', 'New York'),
-  (1, 'IN', 'Indiana'),
-  (67, 'GE', 'General Electric'),
-  (67, 'GM', 'General Motors'),
-  (67, '3M', 'Minnesota Mining and Manufacturing Company'),
-  (67, 'HP', 'Hewlett-Packard');
+	(0, '1', 'State names & abbreviations'),
+	(0, '67', 'Companies with 2-letter abbreviations'),
+	(1, 'CA', 'California'),
+	(1, 'NY', 'New York'),
+	(1, 'IN', 'Indiana'),
+	(67, 'GE', 'General Electric'),
+	(67, 'GM', 'General Motors'),
+	(67, '3M', 'Minnesota Mining and Manufacturing Company'),
+	(67, 'HP', 'Hewlett-Packard');
   
 */
 
--- DROP TABLE IF EXISTS `PublicFacingData`.`Metadata`;
+DROP TABLE IF EXISTS `PublicFacingData`.`Metadata`;
 
 CREATE TABLE `PublicFacingData`.`Metadata` (
-  `GROUP` INT NOT NULL,
-  `ABBR` VARCHAR(2) NOT NULL,
-  `DESC` VARCHAR(100) NULL,
-  PRIMARY KEY (`GROUP`, `ABBR`)
+	`GROUP` INT NOT NULL,
+	`ABBR` VARCHAR(2) NOT NULL,
+	`DESC` VARCHAR(100) NULL,
+	PRIMARY KEY (`GROUP`, `ABBR`)
 );
 
--- SELECT * FROM `PublicFacingData`.`Metadata`;
+INSERT INTO `PublicFacingData`.`Metadata` (`GROUP`, `ABBR`, `DESC`) VALUES
+	(0, '1', 'State names & abbreviations'),
+	(0, '2', 'Institution ownership and financial structure');
+
+INSERT INTO `PublicFacingData`.`Metadata` (`GROUP`, `ABBR`, `DESC`)
+SELECT 1, Codevalue, LEFT(valueLabel, 100) FROM ipeds_2024_db.valuesets22
+	where varName = "STABBR"
+	order by Codevalue;
+
+
+SELECT * FROM `PublicFacingData`.`Metadata`;
