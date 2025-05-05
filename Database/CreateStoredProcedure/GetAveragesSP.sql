0fb83300744ebf2a96f021de94ee090960cf2f78
Ethan Liu <liuj15@rose-hulman.edu>
2025-05-05 13:56:51 -0400

    Add new averages table; modify stored procedure

--- Database/CreateStoredProcedure/GetAveragesSP.sql
+++ Database/CreateStoredProcedure/GetAveragesSP.sql
@@ -8,13 +8,7 @@ delimiter $$
 -- 
 CREATE PROCEDURE GetAverages ()
 BEGIN
-	SELECT "AveragePayMen" as "Name", sum(SALARYTOTACDM)/sum(ACDPOPM) as "Value" FROM merged_ipeds_css_irs.GenderFair2024
-    UNION ALL
-	SELECT "AveragePayWomen" as "Name", sum(SALARYTOTACDF)/sum(ACDPOPF) as "Value" FROM merged_ipeds_css_irs.GenderFair2024
-    UNION ALL
-	SELECT "AverageHateCrime" as "Name", 1000*sum(YEARLYHATECRIME)/sum(ENROLLTOT) FROM merged_ipeds_css_irs.GenderFair2024
-    UNION ALL
-	SELECT "AverageVAWA" as "Name", 1000*sum(YEARLYVAWA)/sum(ENROLLTOT) FROM merged_ipeds_css_irs.GenderFair2024;
+    SELECT Name, Value FROM Averages;
 END$$
 
 delimiter ;
