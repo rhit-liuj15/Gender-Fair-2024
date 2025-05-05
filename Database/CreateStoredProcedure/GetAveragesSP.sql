bab2c7c52fab017da338ec7ea6be9579954eed4c
Ethan Liu <liuj15@rose-hulman.edu>
2025-05-05 01:33:56 -0400

    Averages now report crimes per 1000 students correctly; using IPEDS population

--- Database/CreateStoredProcedure/GetAveragesSP.sql
+++ Database/CreateStoredProcedure/GetAveragesSP.sql
@@ -12,9 +12,9 @@ BEGIN
     UNION ALL
 	SELECT "AveragePayWomen" as "Name", sum(SALARYTOTACDF)/sum(ACDPOPF) as "Value" FROM merged_ipeds_css_irs.GenderFair2024
     UNION ALL
-	SELECT "AverageHateCrime" as "Name", sum(YEARLYHATECRIME)/sum(ENROLLTOT) FROM merged_ipeds_css_irs.GenderFair2024
+	SELECT "AverageHateCrime" as "Name", 1000*sum(YEARLYHATECRIME)/sum(ENROLLTOT) FROM merged_ipeds_css_irs.GenderFair2024
     UNION ALL
-	SELECT "AverageVAWA" as "Name", sum(YEARLYVAWA)/sum(ENROLLTOT) FROM merged_ipeds_css_irs.GenderFair2024;
+	SELECT "AverageVAWA" as "Name", 1000*sum(YEARLYVAWA)/sum(ENROLLTOT) FROM merged_ipeds_css_irs.GenderFair2024;
 END$$
 
 delimiter ;
